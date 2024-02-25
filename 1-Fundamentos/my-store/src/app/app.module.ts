d33632271ddf7fdaff5a637ba0d219039240d4d0
cevadev <carlos.villanueva.altuna@gmail.com>
2024-02-24 21:06:32 -0300

    Curso Fundamentos Angular - Styles

--- 1-Fundamentos/my-store/src/app/app.module.ts
+++ 1-Fundamentos/my-store/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Fundamentos } from './fundamentos.clases';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Fundamentos],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [Fundamentos],
 })
 export class AppModule {}
